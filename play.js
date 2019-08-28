c2a9d9c3b0f81c0049ec0b553c893324a9fa9cdf
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-08-28 17:23:01 +0000

    added input file with user input functions and connected them to play

--- play.js
+++ play.js
@@ -1,17 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
 
-const handleUserInput = ('data', (data) => {
-  if (data == "CONTROL+C") {
-    stdin.destroy();
-  }
-})
+setupInput();
