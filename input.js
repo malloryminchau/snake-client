e8bc7373f6d2307db699cc1de22c3cf42f2a2f42
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-08-28 17:48:44 +0000

    changed input function to use wasd

--- input.js
+++ input.js
@@ -1,10 +1,29 @@
+let connection;
 const handleUserInput = ('data', (data) => {
   if (data == "\u0003") {
     process.exit();
   }
+  if (data == '\u0077') {
+    // console.log('up');
+    connection.write('Move: up')
+  }
+  if (data == '\u0073') {
+    // console.log('down');
+    connection.write('Move: down');
+  }
+  if (data == '\u0061') {
+    // console.log('left');
+    connection.write('Move: left');
+  }
+  if (data == '\u0064') {
+    // console.log('right');
+    connection.write('Move: right');
+  }
+  
 })
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
