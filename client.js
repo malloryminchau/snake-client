d8e1444bb2044670dbf5cf85b28db3cf5d7771bf
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-08-28 16:32:52 +0000

    added some direction commands to client file

--- client.js
+++ client.js
@@ -12,6 +12,20 @@ const connect = function() {
   conn.on('connect', () => {
     conn.write('Name: MAL');
   })
+  conn.on('connect', () => {
+    conn.write("Move: up");
+  })
+  conn.on('connect', () => {
+    setTimeout (() => {
+      conn.write("Move: left");
+    }, 1000);
+  })
+  conn.on('connect', () => {
+    setInterval(() => {
+      conn.write("Move: up");
+    }, 500);
+    
+  })
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('data', (data) => {
