4546bd614adefaf3def7fa946e33afd75a162610
Mallory Minchau <mallory.m.minchau@gmail.com>
2019-08-28 17:05:32 +0000

    added userinput and cancel using ctrl c functions to play

--- play.js
+++ play.js
@@ -1,3 +1,17 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = ('data', (data) => {
+  if (data == "CONTROL+C") {
+    stdin.destroy();
+  }
+})
